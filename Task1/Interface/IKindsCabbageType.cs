24777cbd3d3a8a983c13ca0d56d506b8ab6bbe23
pavel mankialevich <pashagozasinet@hotmail.com>
2017-10-18 00:18:46 +0200

    Add Interface and Folder

---
+++ Task1/Interface/IKindsCabbageType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Interface
+{
+    interface IKindsCabbageType
+    {
+        int Сauliflower { get; }
+        int PekingCabbage { get; }
+        int WhiteCabbage { get; }
+    }
+}
---
+++ Task1/Interface/IKindsTomatoType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Interface
+{
+    interface IKindsTomatoType
+    {
+        int Tomatoes { get; }
+        int Eggplant { get; }
+        int BellPeppers { get; }
+    }
+}
