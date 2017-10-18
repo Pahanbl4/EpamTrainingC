8f5760bde3f8ad343fe05cfdc061cb843cb3a2f6
pavel mankialevich <pashagozasinet@hotmail.com>
2017-10-18 14:35:14 +0200

    add basic classes

---
+++ Task1/Classes/BasicSalad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1.Interface;
+
+namespace Task1.Classes
+{
+    class BasicSalad : IBasicSalad
+    {
+        public string Name { get; set; }
+
+        public double Weight { get; set; }
+
+        public double Caloricity { get; set; }
+    }
+}
