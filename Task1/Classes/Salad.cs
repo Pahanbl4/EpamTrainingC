e6875cbdd7441add28905d35fe0295ed218dbe13
pavel mankialevich <pashagozasinet@hotmail.com>
2017-10-18 16:18:25 +0200

    Sort By Weight

---
+++ Task1/Classes/Salad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1.Interface;
+
+namespace Task1.Classes
+{
+    class Salad
+    {
+        public ICollection<IBasicSalad> Items { get; protected set; }
+        public string Name { get; protected set; }
+
+        public Salad(string name,ICollection<IBasicSalad> items)
+        {
+            Name = name;
+            Items = items;
+        }
+
+        public void SortByWeight()
+        {
+            var result = Items.OrderBy(items => items.Weight).Select(items => items);
+            foreach (var u in result) { u.ToString(); }
+        }
+    }
+}
